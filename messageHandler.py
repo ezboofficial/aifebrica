ccb1f43891e9e722bcb313e89c3b7ce1dbe0fbe9
EzBo <ezboofficial@gmail.com>
2025-06-23 07:29:04 +0600

    Update messageHandler.py

--- messageHandler.py
+++ messageHandler.py
@@ -1,4 +1,3 @@
-import os
 import logging
 import requests
 from io import BytesIO
@@ -31,7 +30,7 @@ logger.addHandler(handler)
 
 # Default settings
 settings = {
-    "shop_name": "Febrica7",
+    "shop_name": "Febrica",
     "shop_number": "+8801709805177",
     "shop_email": "developerabdurrahman88@gmail.com",
     "currency": "BDT",
